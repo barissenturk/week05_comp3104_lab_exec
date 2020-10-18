0fbf1e00b94af60b7296b7628222f1c23e89aea0
barissenturk <barissenturk4@gmail.com>
2020-10-19 01:27:57 +0300

    adding all changes at once which includes package.json work and app src

---
+++ build/precache-manifest.ee4b7e05575ad94bbf83615c146ef9ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b911ebd6d16f7d77395",
+    "url": "/static/css/main.a4fdf0c1.chunk.css"
+  },
+  {
+    "revision": "9b911ebd6d16f7d77395",
+    "url": "/static/js/main.9b911ebd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "31c97ccfcd10f8d745552fb361d08786",
+    "url": "/index.html"
+  }
+];
